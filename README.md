44f07d98c6bd16de6e7c46832bc5c0193efb9de2
jochen <jochen@byte23.net>
2024-12-13 15:16:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# siebensachen
+
+This repo is the frontend of the final project of my journey at WBS Coding School.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1>siebensachen</h1>
+        </>
+    );
+}
+
+export default App;
