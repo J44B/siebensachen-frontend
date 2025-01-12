2bcd2b40af4e34c70018ed5df7cbd2a2c6fedc41
jochen <jochen@byte23.net>
2025-01-12 12:11:26 +0100

    fixed route for EditEventPage

--- src/pages/EditEventPage.jsx
+++ src/pages/EditEventPage.jsx
@@ -18,18 +18,18 @@ import axios from 'axios';
 import { EventForm } from '../components/indexComponents.js';
 
 function EditEventPage() {
-    const { id } = useParams();
+    const { eventId } = useParams();
     const [event, setEvent] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
-
+    console.log('EventId: ', eventId);
     useEffect(() => {
         async function fetchEventData() {
             try {
-                console.log(`Fetching event data for id: ${id}`);
+                console.log('EventId: ', eventId);
                 // Fetch event details
                 const response = await axios.get(
-                    `${import.meta.env.VITE_API_URL}/events/${id}`,
+                    `${import.meta.env.VITE_API_URL}/events/${eventId}`,
                 );
                 setEvent(response.data);
                 setLoading(false);
@@ -41,7 +41,7 @@ function EditEventPage() {
             }
         }
         fetchEventData();
-    }, [id]);
+    }, [eventId]);
 
     if (loading) return <p>EditEventPage says: Loading...</p>;
     if (error) return <p>EditEventPage says: Error loading event.</p>;
